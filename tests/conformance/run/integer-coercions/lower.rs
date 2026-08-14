b14a68bc13837a1f067c8f3ef3687b9609d0e820
Streaky <gfyopptwrhzy@streaky.me>
2026-08-15 00:00:57 +0100

    Cover grouped coercion receiver lowering

--- tests/conformance/run/integer-coercions/lower.rs
+++ tests/conformance/run/integer-coercions/lower.rs
@@ -4,7 +4,7 @@
 fn main() {
     let value: terrane_int_support::Int = terrane_int_support::Int::from(300_i128);
     let exact: i8 = terrane_int_support::unwrap_or_fail(terrane_int_support::coerce::<i8>(&(120)));
-    let wrapped: u8 = terrane_int_support::wrapping_coerce::<u8>(&(value));
+    let wrapped: u8 = terrane_int_support::wrapping_coerce::<u8>(&((value.clone() + terrane_int_support::Int::from(0_i128))));
     let saturated: u8 = terrane_int_support::saturating_coerce::<u8>(&(value));
     println!("{}", terrane_scalar_support::scalar_text(&(exact)));
     println!("{}", terrane_scalar_support::scalar_text(&(wrapped)));
